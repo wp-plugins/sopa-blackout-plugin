eb6179fac5cbca19b9054fa0792ffd96a9b309b9
cgrymala <cgrymala@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-14 22:13:20 +0000

    Removed logos from footer Added i18n to SOPA page Updated readme file
    git-svn-id: https://plugins.svn.wordpress.org/sopa-blackout-plugin/trunk@489982 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -12,6 +12,16 @@ This plugin allows you to set SOPA blackout dates for your WordPress website. SE
 
 This plugin allows you to set SOPA blackout dates for your WordPress website, as well as a variety of options on who the anti-SOPA is shown too. You can have it shown instead of your site for any visitor, you can only show it the first time then let your visitors continue to the site, plus a lot more. This plugin is SEO friendly with temporary redirects being used.
 
+By default, this plugin will automatically redirect visitors from your site's home page to a "Stop SOPA" message on Jan. 18 and 23, 2012. After the visitor has seen the Stop SOPA message, they will be able to continue to your site and will not see the message again during that browser session. There are a number of options available to customize the way the plugin behaves:
+
+1. Blackout Dates - if you would like to change the dates on which the Stop SOPA message is displayed, you can enter a comma-separated list of dates (in YYYY-MM-DD format)
+2. Remove backlinks to plugin sponsors - the default configuration of the Stop SOPA message displays a link to the 3 contributors/sponsors of this plugin. You can disable those by checking this box
+3. Show the SOPA message to visitors the first time they visit your site, no matter which page they land on? - By default, only the front page and the posts page redirect visitors to the SOPA message. If you check this box, all pages will redirect the first time they visit your site.
+4. Don't allow visitors to view the regular site when the SOPA message is active: - With the default configuration, this plugin sets a session cookie when a visitor sees the SOPA message, so that they won't see the message again until they close their browser. If you check this option, they will see the message every time they visit one of the affected pages, no matter how many times they've already seen it.
+5. Link to the following page with the "Continue to site" link - If the "Continue to site" link is displayed on the SOPA page (in certain configurations, it won't be), you can choose which page on your site that link leads to (for instance, if you have the plugin configured not to set a cookie, then you should point visitors to a page other than your home page, since they won't see your site if they try to visit the home page).
+6. Use the following page for the SOPA message: - By default, the plugin redirects to the actual PHP file that displays the SOPA message. You can choose to create a new page in WordPress to display the SOPA message, or choose to display the SOPA message in place of one of your existing pages.
+
+When you deactivate this plugin, all of its settings will be removed. In addition, if you chose to create a new page, this plugin will automatically delete that page.
 
 == Installation ==
 
@@ -35,6 +45,11 @@ A. You bet! Take a look here for a demo http://www.wpsopaplugin.com/stop-sopa/
 
 
 == Changelog ==
+
+= 1.0.3 =
+
+* Added i18n to the Stop SOPA message page
+* Removed logos from footer
 
 = 1.0.2 =
 
--- stop-sopa.php
+++ stop-sopa.php
@@ -23,12 +23,12 @@ $continue_link .= '">' . __( 'Continue to site.' ) . '</a>';
 	</head>
 	<body>
 		<div class="container_12">
-			<h1 class="grid_12 img"><a href="http://downagainstsopa.com"><img src="<?php echo plugins_url( 'images/header.png', __FILE__ ) ?>" alt="Down Against SOPA" /></a><img src="<?php echo plugins_url( 'images/header2.png', __FILE__ ) ?>" alt="This Website is Down Today" /><a href="http://wordpress.org/extend/plugins/sopa-blackout-plugin/"><img src="<?php echo plugins_url( 'images/header3.png', __FILE__ ) ?>" alt="Get the WordPress Plugin" /></a></h1>
+			<h1 class="grid_12 img"><a href="http://downagainstsopa.com"><img src="<?php echo plugins_url( 'images/header.png', __FILE__ ) ?>" alt="<?php _e( 'Down Against SOPA' ) ?>" /></a><img src="<?php echo plugins_url( 'images/header2.png', __FILE__ ) ?>" alt="This Website is Down Today" /><a href="http://wordpress.org/extend/plugins/sopa-blackout-plugin/"><img src="<?php echo plugins_url( 'images/header3.png', __FILE__ ) ?>" alt="<?php _e( 'Get the WordPress Plugin' ) ?>" /></a></h1>
 			<div class="clear"></div>
 
 			<p class="grid_8 white img"><img src="<?php echo plugins_url( 'images/capitol.jpg', __FILE__ ) ?>" alt="" /></p>
-			<p class="grid_6 white censor"><span>If Congress passes SOPA, the Internet will be permanently censored.</span><br /><a class="cta" href="http://downagainstsopa.com/takeaction.php"><span>Take Action Now</span></a></p>
-			<p class="grid_4 banner">Congress is about to vote on the Stop Online Piracy Act (SOPA). If passed, this bill will allow the United States government to censor the Internet. <br /><a href="http://downagainstsopa.com/takeaction.php">Join the fight against it!</a>
+			<p class="grid_6 white censor"><span><?php _e( 'If Congress passes SOPA, the Internet will be permanently censored.' ) ?></span><br /><a class="cta" href="http://downagainstsopa.com/takeaction.php"><span><?php _e( 'Take Action Now' ) ?></span></a></p>
+			<p class="grid_4 banner"><?php printf( __( 'Congress is about to vote on the Stop Online Piracy Act (SOPA). If passed, this bill will allow the United States government to censor the Internet. <br /><a href="%s">Join the fight against it!</a>' ), 'http://downagainstsopa.com/takeaction.php' ) ?>
 <?php 
 /* If just the home page should redirect and either the cookie is turned on or the site link is something other than the home page, show the Continue to site link */ 
 if ( empty( $sopa_opts['no_cookie'] ) ) { 
@@ -41,23 +41,23 @@ if ( empty( $sopa_opts['no_cookie'] ) ) {
 			<div class="border_2"></div>
 			<div class="grid_12 trio">	
 				<div class="grid_4 alpha">
-					<h2>Misdirected Legislation</h2>
-					<p>Although SOPA advocates claim it targets foreign pirates, <a href="https://www.eff.org/deeplinks/2011/11/whats-blacklist-three-sites-sopa-could-put-risk">the Electronic Frontier Foundation reports</a> that &ldquo;broad definitions and vague language&rdquo; allow the bill to shut down legitimate US websites without due process. Among these sites: Etsy, Flickr, and Vimeo.</p>
+					<h2><?php _e( 'Misdirected Legislation' ) ?></h2>
+					<p><?php printf( __( 'Although SOPA advocates claim it targets foreign pirates, <a href="%s">the Electronic Frontier Foundation reports</a> that &ldquo;broad definitions and vague language&rdquo; allow the bill to shut down legitimate US websites without due process. Among these sites: Etsy, Flickr, and Vimeo.' ), 'https://www.eff.org/deeplinks/2011/11/whats-blacklist-three-sites-sopa-could-put-risk' ) ?></p>
 				</div>
 				<div class="grid_4">
-					<h2>Uninformed Support</h2>
-					<p>SOPA supporters in Congress actively avoided feedback from <a href="https://action.eff.org/o/9042/p/dia/action/public/?action_KEY=8173">public interest groups</a>, Internet <a href="http://www.businessinsider.com/paul-graham-just-exiled-these-big-companies-from-y-combinator-demo-days-demo-days-2011-12">investors</a> and professionals, <a href="http://www.tumblr.com/protect-the-net">technology companies</a>, and <a href="http://filmfwd.com/2011/12/no-protection-filmmaker-against-sopa/">independent artists</a>. They were too busy listening to lobbyists to hear the widespread outrage over the bill&rsquo;s many flaws.</p>     
+					<h2><?php _e( 'Uninformed Support' ) ?></h2>
+					<p><?php printf( __( 'SOPA supporters in Congress actively avoided feedback from <a href="%s">public interest groups</a>, Internet <a href="%s">investors</a> and professionals, <a href="%s">technology companies</a>, and <a href="%s">independent artists</a>. They were too busy listening to lobbyists to hear the widespread outrage over the bill&rsquo;s many flaws.' ), 'https://action.eff.org/o/9042/p/dia/action/public/?action_KEY=8173', 'http://www.businessinsider.com/paul-graham-just-exiled-these-big-companies-from-y-combinator-demo-days-demo-days-2011-12', 'http://www.tumblr.com/protect-the-net', 'http://filmfwd.com/2011/12/no-protection-filmmaker-against-sopa/' ) ?></p>     
 				</div>
 				<div class="grid_4 omega">
-					<h2>What You Can Do</h2>
-					<p>It is crucial that we demonstrate our opposition. Let Congress know you oppose SOPA. <a href="https://www.senate.gov/general/contact_information/senators_cfm.cfm">Look up your senators&rsquo; numbers</a> and call them. Or <a href="https://www.senate.gov/general/contact_information/senators_cfm.cfm">send a message</a> announcing your opposition. Working together, we can protect your First Ammendment rights.</p>     
+					<h2><?php _e( 'What You Can Do' ) ?></h2>
+					<p><?php printf( __( 'It is crucial that we demonstrate our opposition. Let Congress know you oppose SOPA. <a href="%s">Look up your senators&rsquo; numbers</a> and call them. Or <a href="%s">send a message</a> announcing your opposition. Working together, we can protect your First Ammendment rights.' ), 'https://www.senate.gov/general/contact_information/senators_cfm.cfm', 'https://www.senate.gov/general/contact_information/senators_cfm.cfm' ) ?></p>     
 				</div>
 			</div>
 			<div class="clear"></div>
 <?php
 if ( ! isset( $sopa_opts ) || 1 != $sopa_opts['backlinks'] ) {
 ?>
-			<p class="grid_12 footer"><a href="http://downagainstsopa.com">Down Against Sopa</a> sponsored by <a href="http://site5.com">Site5</a> &middot; Copyright &copy; 2012 <a href="http://ctidd.com">Chris Tidd</a>; <a href="https://www.gnu.org/copyleft/gpl.html">code released under GPLv3</a> unless otherwise licensed.<br />WordPress Development by <a href="http://ten-321.com/">Ten-321 Enterprises</a><br /><br /><a href="http://site5.com"><img src="<?php echo plugins_url( 'images/logo_site5.png', __FILE__ ) ?>" alt="Site5" /></a><a href="http://ctidd.com"><img src="<?php echo plugins_url( 'images/logo_dwt.png', __FILE__ ) ?>" alt="Design with Tidd" /></a></p>
+			<p class="grid_12 footer"><a href="http://downagainstsopa.com"><?php _e( 'Down Against Sopa' ) ?></a> <?php _e( 'sponsored by' ) ?> <a href="http://site5.com">Site5</a> &middot; <?php _e( 'Copyright' ) ?> &copy; 2012 <a href="http://ctidd.com">Chris Tidd</a>; <a href="https://www.gnu.org/copyleft/gpl.html"><?php _e( 'code released under GPLv3' ) ?></a> <?php _e( 'unless otherwise licensed.' ) ?><br /><?php _e( 'WordPress Development by' ) ?> <a href="http://ten-321.com/">Ten-321 Enterprises</a></p>
 <?php
 }
 ?>
